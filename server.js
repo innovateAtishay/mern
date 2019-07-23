caf15bc37ebb186cd3133d9771c6973587f6d6d7
innovateAtishay <atishay.khare@innovationm.com>
2019-07-23 18:40:02 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+//connect Database
+connectDB();
+
+app.get('/index', (req, res) => res.send('Api Running'));
+
+//Define route
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/post', require('./routes/api/post'))
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=> console.log(`Server Started on Port ${PORT}`));
